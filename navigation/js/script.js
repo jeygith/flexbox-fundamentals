060c6dfb14b053733e80e2a88509f67123286497
Jeff Githiri <geoffrey.githiri@andela.com>
2019-11-14 17:20:53 +0300

    navigation css refactor

--- navigation/js/script.js
+++ navigation/js/script.js
@@ -1,5 +1,32 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
+    $('#magnify').click(function () {
+        $(this).hide();
+        $('#search').toggle('scale');
+        $('#search').css('display', 'block')
+    });
 
 });
 
+
+
+Vue.component('button-counter', {
+    data: function() {
+        return {
+            count: 0
+        }
+    },
+    template: '<button v-on:click="count++">You clicked me {{ count }} times.</button>'
+})
+
+
+
+
+Vue.Component('button-counter', {
+    data: function() {
+        return {
+            count: 0
+        }
+    },
+    template: '<button v-on:click="count++">You clicked me {{ count }} times.</button>'
+})
